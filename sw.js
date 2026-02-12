34fd5150341cf41129b1d061ae587b35dedbd0c7
tymo1985-debug <tymo1985@googlemail.com>
2026-02-13 00:18:39 +0100

    Bump cache version to service-pro-v60.1
    Updated cache version for service worker.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
 // === Версия кэша (обновляйте при каждом релизе) ===
-const CACHE_NAME = 'service-pro-v59.6';
+const CACHE_NAME = 'service-pro-v60.1';
 
 // === Предкэшируемые ресурсы ===
 const URLS_TO_CACHE = [
